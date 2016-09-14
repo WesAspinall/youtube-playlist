628214d06ae70227875d2ceaf9a0571a7729cad1
Wesley Aspinall <wra427@gmail.com>
2016-09-13 23:54:51 -0400

    removed git from constants

--- js/app-core/constants/fileserver.constant.js
+++
@@ -1,8 +0,0 @@
-export default {
-    PARAMS: {
-        
-        API_KEY: 'AIzaSyDwUxjAYUo4MsgECo9xdSMSKZTZI3hZHos',
-        LIST_ID: 'PLElfEOBGqoUkvqwPb31hWZ3ngAfCBUc5Z',
-        CHANNEL_ID: 'UCTgg3k-iHG_OwbR79jDmmLA'       
-    }
-};
